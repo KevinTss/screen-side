d77c2dc3b1e63b323f0c4dec3ffcb14cc69b2449
KevinTss <kevin.tassi@gmail.com>
2021-12-18 13:44:01 +0100

    Add Google Analytics script

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,6 +13,20 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" type="image/x-icon" href="favicon.ico" />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          {!!process.env.GA_TRACKING_ID && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${process.env.GA_TRACKING_ID}',{page_path: window.location.pathname});`,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />
