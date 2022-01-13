040920d6a316c347fac66eec5ef3cbfbc068ebe3
KevinTss <kevin.tassi@gmail.com>
2022-01-13 18:01:57 +0100

    Fix ssr styling with styled component

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,11 +1,32 @@
 // https://nextjs.org/docs/advanced-features/custom-document
 import Document, { Html, Head, Main, NextScript } from "next/document"
+import { ServerStyleSheet } from "styled-components"
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-    return { ...initialProps }
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
